af683872acec5529cc57995395abdf93470355f7
SusanosHermanes <140815175+SusanosHermanes@users.noreply.github.com>
2026-04-02 01:19:31 +0800

    Have done Menu for the game. Still need to update the class promptActionChoice(Player player), as I need the Action classes which have not been implemented yet

--- src/ui/CombatLogger.java
+++ src/ui/CombatLogger.java
@@ -4,10 +4,15 @@ import src.effects.StatusEffect;
 import src.actions.Action;
 import src.character.Combatant;
 
+import java.util.List;
+
 public class CombatLogger {
     public void log(String message) {}
     public void logAction(Action action, Combatant user) {}
     public void logDamage(int damage, Combatant target) {}
     public void logHealing(int heal, Combatant target) {}
     public void logEffect(StatusEffect effect, Combatant target) {}
+    public void displayCombatLog(String message) {}
+    public void displayCombatStatus(List<Combatant> players, List<Combatant> enemies) {}
+    public void displayGameOver(boolean victory) {}
 }
