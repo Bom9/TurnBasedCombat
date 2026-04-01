66e4088ff3378bbb162d272a485cc05115700eb4
yzzzap <zhengtingyap@gmail.com>
2026-04-01 17:46:46 +0800

    Update Defend.java

--- src/actions/Defend.java
+++ src/actions/Defend.java
@@ -1,4 +1,15 @@
 package src.actions;
 
-public class Defend {
+public class Defend implements Action{
+  @Override
+  public void execute(Combatant user, List<Combatant> targets){
+    StatusEffect defenseBoost = new DefenseBoost(2,10);
+    user.addEffect(defenseBoost);
+    System.out.println(user.getName() + " defends! Defense increased by 10 for 2 turns.");
+  }
+
+  @Override
+  public String getName(){
+    return "Defend";
+  }
 }
