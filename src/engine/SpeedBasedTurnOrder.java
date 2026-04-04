677990437b7c1039bd6508f1654f7c90b5065970
Bom9 <jianfa1222@gmail.com>
2026-04-04 11:19:16 +0800

    update on speed strategy

--- src/engine/SpeedBasedTurnOrder.java
+++ src/engine/SpeedBasedTurnOrder.java
@@ -1,16 +1,19 @@
 package src.engine;
 
 import src.character.Combatant;
-import java.util.Collection;
-import java.util.Comparator;
 import java.util.List;
+import java.util.ArrayList;
 
 public class SpeedBasedTurnOrder implements  TurnOrderStrategy{
     @Override
-    public void sort(List<Combatant> combatants){
-        /* sort in descending order using lambda expression
+    public List<Combatant> sortCombatants(List<Combatant> combatants){
+        /*
+            sort in descending order using lambda expression
            based on the speed, if c1 is the first argument, then is ascending
+           return the sorted list
          */
-        combatants.sort((c1, c2) -> Integer.compare(c2.getSpeed(), c1.getSpeed()));
+        List<Combatant> sorted = new ArrayList<>(combatants);
+        sorted.sort((c1, c2) -> Integer.compare(c2.getSpeed(), c1.getSpeed()));
+        return sorted;
     }
 }
--- src/engine/TurnOrderStrategy.java
+++ src/engine/TurnOrderStrategy.java
@@ -2,6 +2,15 @@ package src.engine;
 import src.character.Combatant;
 
 import java.util.List;
+/*
+an interface having the method/methods to sort the combatant
+currently only sorting based on speed,
+could be extended by introducing new method for the implementing class to implement
+ */
 public interface TurnOrderStrategy {
-    void sort(List<Combatant> combatants);
+    /*
+    interface method for different sub class to implement
+    currently, only one sub class, SpeedBasedTurnOrder
+     */
+    List<Combatant> sortCombatants(List<Combatant> combatants);
 }
