de713376ffad16477df1cdedb189d8926c8fe11e
Bom9 <jianfa1222@gmail.com>
2026-04-04 11:07:48 +0800

    done level, need update Goblin & Wolf

--- src/level/LevelManager.java
+++ src/level/LevelManager.java
@@ -1,15 +1,60 @@
 package src.level;
 
-import src.character.Combatant;
+import jdk.jshell.execution.JdiDefaultExecutionControl;
+import src.character.*;
 import src.ui.GameConfig;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class LevelManager {
-    private GameConfig difficulty;
-    protected SpawnConfig easySpawnConfig;
-    protected SpawnConfig mediumSpawnConfig;
-    protected SpawnConfig hardSpawnConfig;
+    //based on the input, initialise the respective SpawnConfig
+    public SpawnConfig buildSpawnConfig(DifficultyLevel difficulty){
+        return switch(difficulty){
+            case EASY -> buildEasy();
+            case MEDIUM -> buildMedium();
+            case HARD -> buildHard();
+        };
+    }
+    /*
+    Setting the SpawnConfig with the correct amount of enemy according to the chosen difficulty level
+     */
+    protected SpawnConfig buildEasy(){
+        List<Enemy> initial = new ArrayList<>();
+        initial.add(new Goblin("Goblin A"));
+        initial.add(new Goblin("Goblin B"));
+        initial.add(new Goblin("Goblin B"));
+        return new SpawnConfig(initial, Collections.emptyList());
+    }
+    protected SpawnConfig buildMedium(){
+        List<Enemy> initial = new ArrayList<>();
+        initial.add(new Goblin("Goblin"));
+        initial.add(new Wolf("Wolf"));
 
+        List<Enemy> backup = new ArrayList<>();
+        backup.add(new Wolf("Wolf A"));
+        backup.add(new Wolf("Wolf B"));
+        return new SpawnConfig(initial, backup);
+    }
+    protected SpawnConfig buildHard(){
+        List<Enemy> initial = new ArrayList<>();
+        initial.add(new Goblin("Goblin A"));
+        initial.add(new Goblin("Goblin B"));
+
+        List<Enemy> backup = new ArrayList<>();
+        backup.add(new Goblin("Goblin C"));
+        backup.add(new Wolf("Wolf A"));
+        backup.add(new Wolf("Wolf B"));
+        return new SpawnConfig(initial, backup);
+    }
+    //Display the description of each difficulty level
+    public String describeSpawn(DifficultyLevel difficulty){
+        return switch(difficulty){
+            case EASY -> "Initial Spawn: 3 Goblins";
+            case MEDIUM -> "Inital Spawn: 1 Goblin + 1 Wolf | Backup Spawn: 2 Wolfs";
+            case HARD -> "Initial Spawn: 2 Goblins | Backup: 1 Goblin = 2 Wolfs";
+        };
+    }
     public List<Combatant> getInitialWave() { return null; }
     public List<Combatant> getBackupWave() { return null; }
     public void setDifficulty(GameConfig level) {}
--- src/level/SpawnConfig.java
+++ src/level/SpawnConfig.java
@@ -1,7 +1,28 @@
 package src.level;
+
+import src.character.Enemy;
+import java.util.List;
+/*
+hold the instance of the configured wave of enemy based on the chosen difficulty
+ */
 public class SpawnConfig {
-    public int goblinCount;
-    public int wolfCount;
-    public int backupGoblinCount;
-    public int backupWolfCount;
+    //immutable, cannot be modified
+    private final List<Enemy> initialSpawn;
+    private final List<Enemy> backupSpawn;
+
+    public SpawnConfig(List<Enemy> initialSpawn, List<Enemy> backupSpawn){
+        /*
+        this.initalSpawn = initialSpawn
+        this method is acceptable but it holds the reference only, might be modified
+         */
+        this.initialSpawn = List.copyOf(initialSpawn); // List.copyOf return a read only list
+        this.backupSpawn = List.copyOf(backupSpawn);
+    }
+    //returning the constructed list
+    public List<Enemy> getInitialSpawn(){
+        return initialSpawn;
+    }
+    public List<Enemy> getBackupSpawn(){
+        return backupSpawn;
+    }
 }
