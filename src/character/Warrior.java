9293aeb074f0440602f2171c302c064e1559f603
yzzzap <zhengtingyap@gmail.com>
2026-04-06 19:51:20 +0800

    added warrior class

--- src/character/Warrior.java
+++ src/character/Warrior.java
@@ -1,4 +1,18 @@
 package src.character;
 
-public class Warrior {
+import action.ShieldBash;
+
+public class Warrior extends Player{
+    public static final int BASE_HP = 260;
+    public static final int BASE_ATK = 40;
+    public static final int BASE_DEF = 20;
+    public static final int BASE_SPD = 30;
+
+    public Warrior(String name){
+        super(name, BASE_HP, BASE_ATK, BASE_DEF, BASE_SPD, new ShieldBash());
+    }
+
+    public Warrior(){
+        this(name: "Warrior");
+    }
 }
