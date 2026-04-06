47d9fceca1efedad0f400026522dcc80eebb2c95
yzzzap <zhengtingyap@gmail.com>
2026-04-06 23:11:19 +0800

    Update Combatant.java

--- src/character/Combatant.java
+++ src/character/Combatant.java
@@ -2,30 +2,103 @@ package src.character;
 import src.items.Item;
 import src.effects.StatusEffect;
 
+import java.util.ArrayList;
 import java.util.List;
-public abstract class Combatant {
-    protected int maxHp;
-    protected int hp;
-    protected int attack;
-    protected int defense;
-    protected int speed;
-    protected List<Item> inventory;
-    protected List<StatusEffect> activeEffects;
-    protected String name;
+import java.util.Iterator;
 
-    public abstract void takeTurn();
+public abstract class Combatant {
+    protected final String name;
+    protected final int maxHP;
+    protected int hp;
+    protected int atk;
+    protected int def;
+    protected final int spd;
+    protected boolean isAlive;
+    protected final List<StatusEffect> activeEffects;
+
+    protected Combatant(String name, int maxHP, int atk, int def, int spd) {
+        this.name         = name;
+        this.maxHP        = maxHP;
+        this.hp           = maxHP;
+        this.atk          = atk;
+        this.def          = def;
+        this.spd          = spd;
+        this.isAlive      = true;
+        this.activeEffects = new ArrayList<>();
+    }
+
+    public void takeDamage(int damage) {
+        int actual = Math.max(0, damage);
+        hp = Math.max(0, hp - actual);
+        if (hp == 0) isAlive = false;
+    }
+
+    public void heal(int amount) {
+        hp = Math.min(maxHP, hp + Math.max(0, amount));
+    }
     
-    public void takeDamage(int damageAmount) {}
-    public void heal(int healAmount) {}
-    public boolean isAlive() { return false; }
-    public void updateEffects() {}
-    public int getSpeed() { return 0; }
-    public int getHp() { return 0; }
-    public int getMaxHp() { return 0; }
-    public int getAttack() { return 0; }
-    public int getDefense() { return 0; }
-    public List<Item> getInventory() { return null; }
-    public void addEffect(StatusEffect effect) {}
-    public void removeEffect(StatusEffect effect) {}
-    public String getName() {}
+    public void addStatusEffect(StatusEffect effect) {
+        effect.onApply(this);
+        activeEffects.add(effect);
+    }
+    public void decrementNonStunEffects() {
+        Iterator<StatusEffect> it = activeEffects.iterator();
+        while (it.hasNext()) {
+            StatusEffect effect = it.next();
+            if (!effect.isTurnBlocking()) {
+                effect.decrementDuration();
+                if (effect.isExpired()) {
+                    effect.onExpire(this);
+                    it.remove();
+                }
+            }
+        }
+    }
+    public boolean processStun() {
+        Iterator<StatusEffect> it = activeEffects.iterator();
+        while (it.hasNext()) {
+            StatusEffect effect = it.next();
+            if (effect.isTurnBlocking()) {
+                effect.decrementDuration();
+                if (effect.isExpired()) {
+                    effect.onExpire(this);
+                    it.remove();
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isProtectedFromAttack() {
+        return activeEffects.stream().anyMatch(StatusEffect::nullifiesEnemyDamage);
+    }
+
+    public int getEffectiveDef() {
+        int bonus = activeEffects.stream()
+                                  .mapToInt(StatusEffect::getDefenseBonus)
+                                  .sum();
+        return def + bonus;
+    }
+    public boolean isStunned() {
+        return activeEffects.stream().anyMatch(StatusEffect::isTurnBlocking);
+    }
+
+    public String             getName()         { return name;         }
+    public int                getMaxHP()        { return maxHP;        }
+    public int                getHP()           { return hp;           }
+    public int                getAtk()          { return atk;          }
+    public int                getDef()          { return def;          }
+    public int                getSpd()          { return spd;          }
+    public boolean            isAlive()         { return isAlive;      }
+    public List<StatusEffect> getActiveEffects(){ return activeEffects; }
+
+    public void setAtk(int atk) { this.atk = atk; }
+
+    public abstract String getStatusString();
+
+    @Override
+    public String toString() {
+        return name + " [HP: " + hp + "/" + maxHP + "]";
+    }
 }
