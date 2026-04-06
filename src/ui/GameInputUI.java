ffa225d0bc0a8f7fd5385477e99b6c79a87b6d56
SusanosHermanes <140815175+SusanosHermanes@users.noreply.github.com>
2026-04-06 17:32:47 +0800

    Added methods for ConsoleGameInput

--- src/ui/GameInputUI.java
+++ src/ui/GameInputUI.java
@@ -1,4 +1,32 @@
 package src.ui;
+import java.util.list;
+import item.Item;
+import action.Action;
+import entity.Combatant;
+import entity.Player;
+import level.DifficultyLevel;
+import src.character.Combatant;
 
 public interface GameInputUI {
+    //Select a class, returns a newly constructed Player instance with class
+    Player promptPlayerSelection();
+
+    // Player picks 2 items, duplicates are allowed, retuurns list of exactly 2 chosen Item instances
+    List<Item> promptItemSelection();
+
+    //Choose Difficulty Level
+    DifficultyLevel promptDifficultySelection();
+
+    //Player turn options, - like a player pokemon battle menu
+    Action promptActionChoice(Player player, List<Action> availableActions);
+
+    //Player chooses enemies to attack
+    Combatant promptTargetSelection(List<Combatant> enemies);
+
+    //Player chooses item to use
+    int promptItemSelection(Player player);
+
+    //Post game options
+    int promptPostGameMenu();
+
 }
