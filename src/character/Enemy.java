73fc2d072324df138ccdd32cf83a307df903f30e
yzzzap <zhengtingyap@gmail.com>
2026-04-09 17:16:21 +0800

    Update Enemy.java
    changed to new class names

--- src/character/Enemy.java
+++ src/character/Enemy.java
@@ -1,12 +1,48 @@
-package src.character;
+package src.entity;
 
-import src.actions.Action;
+import java.util.List;
+
+import src.action.Action;
 import src.actions.EnemyActionStrategy;
+import src.actions.EnemyStrategy;
 
 public abstract class Enemy extends Combatant {
-    protected EnemyActionStrategy actionStrategy;
+    protected EnemyStrategy strategy;
 
-    public Action decideAction() { return null; }
-    public EnemyActionStrategy getActionStrategy() { return null; }
-    public void setActionStrategy(EnemyActionStrategy strategy) {}
+    protected Enemy(String name, int maxHP, int atk, int def, int spd, EnemyStrategy strategy) {
+        super(name, maxHP, atk, def, spd);
+        this.strategy = strategy;
+    }
+    
+    public Action decideAndGetAction(List<Combatant> players) {
+        return strategy.decideAction(this, players);
+    }
+
+    public void setStrategy(EnemyStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public EnemyStrategy getStrategy() {
+        return strategy;
+    }
+
+    @Override
+    public String getStatusString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(name)
+          .append(" | HP: ").append(hp).append("/").append(maxHP)
+          .append(" | ATK: ").append(atk)
+          .append(" | DEF: ").append(def)
+          .append(" | SPD: ").append(spd);
+        if (!isAlive) {
+            sb.append(" [ELIMINATED]");
+        } else if (isStunned()) {
+            sb.append(" [STUNNED]");
+        }
+        if (!activeEffects.isEmpty() && isAlive) {
+            sb.append(" Effects: ");
+            activeEffects.forEach(e -> sb.append(e.getName()).append(" "));
+        }
+        return sb.toString();
+    }
 }
