535098b2da5c5021bfed012dff6231535024fc7c
yzzzap <zhengtingyap@gmail.com>
2026-04-09 16:40:55 +0800

    Update Wolf.java

--- src/character/Wolf.java
+++ src/character/Wolf.java
@@ -1,4 +1,18 @@
 package src.character;
 
-public class Wolf {
+import strategy.BasicEnemyStrategy;
+public class Wolf extends Enemy {
+
+    public static final int BASE_HP  = 40;
+    public static final int BASE_ATK = 45;
+    public static final int BASE_DEF = 5;
+    public static final int BASE_SPD = 35;
+
+    public Wolf(String label) {
+        super(label, BASE_HP, BASE_ATK, BASE_DEF, BASE_SPD, new BasicEnemyStrategy());
+    }
+
+    public Wolf() {
+        this("Wolf");
+    }
 }
