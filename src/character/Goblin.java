09c45dbd35f946fee2dcac83114965dc7785b487
yzzzap <zhengtingyap@gmail.com>
2026-04-09 16:34:46 +0800

    Update Goblin.java

--- src/character/Goblin.java
+++ src/character/Goblin.java
@@ -1,4 +1,18 @@
 package src.character;
+import strategy.BasicEnemyStrategy;
 
 public class Goblin {
+
+    public static final int BASE_HP  = 55;
+    public static final int BASE_ATK = 35;
+    public static final int BASE_DEF = 15;
+    public static final int BASE_SPD = 25;
+
+    public Goblin(String label) {
+        super(label, BASE_HP, BASE_ATK, BASE_DEF, BASE_SPD, new BasicEnemyStrategy());
+    }
+
+    public Goblin() {
+        this("Goblin");
+    }
 }
