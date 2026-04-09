e33179f9773ef71ec4733a1a851e775446851af4
Samrithi Satish <samrithisatish@Samrithis-MacBook-Air.local>
2026-04-09 21:37:42 +0800

    Updated Item interface to support multiple targets

--- src/items/Item.java
+++ src/items/Item.java
@@ -1,10 +1,10 @@
 package src.items;
 
-import src.character.Combatant;
+import src.entity.Combatant;
+import java.util.List;
 
 public interface Item {
-
     String getName();
     String getDescription();
-    void use (Combatant user,Combatant target);
+    void use(Combatant user, List<Combatant> targets);
 }
