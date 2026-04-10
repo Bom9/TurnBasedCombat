912ee42b29bbb3c11da6bb03c85f164299608673
Samrithi Satish <samrithisatish@Samrithis-MacBook-Air.local>
2026-04-10 11:32:18 +0800

    Add Potion item to heal player by 100 HP

--- src/items/Potion.java
+++ src/items/Potion.java
@@ -1,48 +1,27 @@
 package src.items;
 
-import src.character.Combatant;
+import character.Combatant;
 import java.util.List;
 
-public class Potion implements Item {
+
+ //Potion heals the user by 100 HP, capped at max HP.
+ 
+public class Potion extends Item {
 
     private static final int HEAL_AMOUNT = 100;
-    private boolean used = false;
 
-    @Override
-    public String getName() {
-        return "Potion";
-    }
-
-    @Override
-    public String getDescription() {
-        return "Heal 100 HP.";
+    public Potion() {
+        super("Potion");
     }
 
     @Override
     public void use(Combatant user, List<Combatant> targets) {
-        if (used) {
-            System.out.println("Potion has already been used.");
-            return;
-        }
-
-        if (user == null) {
-            System.out.println("No user selected.");
-            return;
-        }
-
-        if (!user.isAlive()) {
-            System.out.println(user.getName() + " is defeated and cannot use items.");
-            return;
-        }
-
-        int beforeHP = user.getHP();
         user.heal(HEAL_AMOUNT);
-        int afterHP = user.getHP();
+    }
 
-        System.out.println(user.getName() + " used Potion! HP: " + beforeHP + " -> " + afterHP);
-
-        used = true;
+    @Override
+    public String getDescription() {
+        return "Heals " + HEAL_AMOUNT + " HP (capped at max HP).";
     }
 }
-
 
