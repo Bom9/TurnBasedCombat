d44ac9834b35219ce35fd09ffd7c714cde91c2fa
Samrithi Satish <samrithisatish@Samrithis-MacBook-Air.local>
2026-04-10 11:18:42 +0800

    Add Stun effect to block turns for 2 turns

--- src/effects/Stun.java
+++ src/effects/Stun.java
@@ -1,7 +1,8 @@
 package src.effects;
 
-import src.character.Combatant;
 
+ //Stun prevents the affected combatant from acting for 2 turns.
+ 
 public class Stun extends StatusEffect {
 
     private static final int STUN_DURATION = 2;
@@ -11,17 +12,12 @@ public class Stun extends StatusEffect {
     }
 
     @Override
-    public void onApply(Combatant target) {
-        System.out.println(target.getName() + " is stunned and cannot act for this turn and the next turn!");
-    }
-
-    @Override
-    public void onExpire(Combatant target) {
-        System.out.println(target.getName() + " is no longer stunned.");
-    }
-
-    @Override
     public boolean isTurnBlocking() {
         return true;
+    }
+
+    @Override
+    public String toString() {
+        return "STUNNED (" + duration + " turn" + (duration == 1 ? "" : "s") + " remaining)";
     }
 }
